049faf9edff3ae14be660450c394d4abded282e1
ttpatel <tirthp1898@gmail.com>
2021-04-27 23:01:09 +0530

    Final commit

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Container, Row, Col, InputGroup, FormControl, Button,search } from 'react-bootstrap'
+import { Container, Row, Col, InputGroup, FormControl, Button} from 'react-bootstrap'
 
 
 function SearchBar({ handleSearch, search, setSearch }) {
