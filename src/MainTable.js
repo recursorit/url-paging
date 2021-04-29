58d3b7c16019cd7ca11468d4e6f3b949ad6b19be
ttpatel <tirthp1898@gmail.com>
2021-04-29 20:47:08 +0530

    Final commit

--- src/MainTable.js
+++ src/MainTable.js
@@ -6,24 +6,23 @@ import PaginationPart from './PaginationPart';
 const MainTable = ({ posts, reset, error }) => {
 
     const [currentPage, setCurrentPage] = useState(1);
-    const [postsPerPage, setPostsPerPage] = useState(5);
-    const [sort, setSort] = useState({ 'avatar_url': null, 'login': null, 'type': null })
-    const [arrow, setArrow] = useState("")
+    const [postsPerPage, setpostsPerPage] = useState(5);
+    const [sort, setSort] = useState({ 'login': null, 'avatar_url': null, 'type': null })
+
     const sorting = (name) => {
 
         const val = sort[name]
-        // if (val == null) {
-        //     sort[name] = ''
-        //     setArrow("")
-        // }
-         if (val === 'asc') {
+        if (val == null) {
+            sort[name] = 'asc'
+
+        }
+        else if (val === 'asc') {
             sort[name] = 'desc'
-            setArrow("↓")
+
         } else {
             sort[name] = 'asc'
-            setArrow("↑")
+
         }
-        console.log('inside sorting')
         setSort({ ...sort })
     }
     console.log(sort)
@@ -31,10 +30,10 @@ const MainTable = ({ posts, reset, error }) => {
     let sortObj = { fields: [], order: [] }
     for (let col in cols) {
         if (sort[cols[col]]) {
-            console.log(cols[col])
             sortObj.fields.push(cols[col])
             sortObj.order.push(sort[cols[col]])
         }
+        console.log(sortObj)
     }
     console.log(sortObj)
     const sortedposts = lodash.orderBy(posts, sortObj.fields, sortObj.order)
@@ -43,7 +42,7 @@ const MainTable = ({ posts, reset, error }) => {
     console.log("sort", sortedposts)
     const goBack = () => {
         setCurrentPage(1)
-        setPostsPerPage(5)
+        setpostsPerPage(5)
         reset()
     }
     const paginate = (pageNumbers) => {
@@ -52,23 +51,23 @@ const MainTable = ({ posts, reset, error }) => {
     }
     const indexOfLastPost = currentPage * postsPerPage
     const indexOfFirstPost = indexOfLastPost - postsPerPage
-    const currentPosts = sortedposts.slice(indexOfFirstPost, indexOfLastPost)
+    const currentposts = sortedposts.slice(indexOfFirstPost, indexOfLastPost)
 
     return (
         <Row className='mt-2 text-center'>
             <Button variant="primary" onClick={goBack} className=" goback m-3 px-3 " >Go Back</Button>
-            {error ? <h5 className='text-center text-danger mt-3'>No records found. </h5> : <Col xs={9} md={12} lg={12}>
+            {error ? <h5 className='text-center text-danger mt-3'>No records found. </h5> : <Col xs={12} md={12} lg={12}>
                 < Table className='p-2' striped bordered >
                     <thead>
                         <tr className='text-center hovereffect'>
-                            <th> <code onClick={() => sorting('avatar_url')}>Avatar  {'avatar_url' ? arrow : null} </code></th>
-                            <th> <code  onClick={() => sorting('login')} >Login  {'login '? arrow : null}</code></th>
-                            <th> <code  onClick={() => sorting('type')} >Type{'type'? arrow : null}</code></th>
+                            <th> <code onClick={() => sorting('avatar_url')}>Avatar  {sort['avatar_url'] === null ? '↑↓' : (sort['avatar_url'] === 'asc' ? '↑' : '↓')} </code></th>
+                            <th> <code onClick={() => sorting('login')} >Login {sort['login'] === null ? '↑↓' : (sort['login'] === 'asc' ? '↑' : '↓')}</code></th>
+                            <th> <code onClick={() => sorting('type')} >Type {sort['type'] === null ? '↑↓' : (sort['type'] === 'asc' ? '↑' : '↓')}</code></th>
                         </tr>
                     </thead>
                     <tbody>
                         {
-                            currentPosts.map(posts => {
+                            currentposts.map(posts => {
                                 return (<tr key={posts.id} className='text-center'>
                                     <td><img src={posts.avatar_url} alt='avtarimg' className='avatar' /></td>
                                     <td className='text-capitalize'>{posts.login}</td>
@@ -80,7 +79,7 @@ const MainTable = ({ posts, reset, error }) => {
                     </tbody>
                 </ Table>
                 <Form className='formgroup'>
-                    <Form.Group controlId='exampleForm.SelectCustom' onChange={(e) => setPostsPerPage(e.target.value)}>
+                    <Form.Group controlId='exampleForm.SelectCustom' onChange={(e) => setpostsPerPage(e.target.value)}>
                         <Form.Label >Rows per page</Form.Label>
                         <Form.Control as='select' custom>
                             <option>5</option>
@@ -90,7 +89,7 @@ const MainTable = ({ posts, reset, error }) => {
                         </Form.Control>
                     </Form.Group>
                 </Form>
-                <PaginationPart  posts={sortedposts} postsPerPage={postsPerPage} paginate={paginate} currentPage={currentPage} setCurrentPage={setCurrentPage} />
+                <PaginationPart posts={sortedposts} postsPerPage={postsPerPage} paginate={paginate} currentPage={currentPage} setCurrentPage={setCurrentPage} />
             </Col>}
         </Row>
     )
